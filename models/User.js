65c52270a1ca63887490390f3a8d5f39b9f4fe09
ozkan <ozkandnz09@gmail.com>
2025-11-27 13:03:50 +0300

    usermodel done

--- models/User.js
+++ models/User.js
@@ -2,33 +2,54 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [
-      /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/,
-      "Please add a valid email",
-    ],
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please add a valid email",
+      ],
+    },
+    password: {
+      type: String,
+    },
+    name: {
+      type: String,
+      maxlenght: 50,
+      minlenght: 3,
+    },
+    login_pin: {
+      type: String,
+      maxlenght: 4,
+      minlenght: 4,
+    },
+    phone_number: {
+      type: String,
+      match: [/^[0-9]{10}$/, "Please add a valid phone number"],
+      unique: true,
+      sparse: true,
+    },
+    date_of_birth: Date,
+    biometricKey: String,
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+    },
+    wrong_pin_attemps: {
+      type: Number,
+      default: 0,
+    },
+    blocked_until_ğin: {
+      type: Date,
+      default: null,
+    },
+    balance: {
+      type: Number,
+      default: 50000.0,
+    },
   },
-  password: {
-    type: String,
-  },
-  name: {
-    type: String,
-    maxlenght: 50,
-    minlenght: 3,
-  },
-  login_pin: {
-    type: String,
-    maxlenght: 4,
-    minlenght: 4,
-  },
-  phone_number: {
-    type: String,
-    match: [/^[0-9]{10}$/, "Please add a valid phone number"],
-    unique: true,
-    sparse: true,
-  },
-});
+  { timestamps: true }
+);
