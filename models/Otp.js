b0d98c2fbe2c883ae62455a6ccfc22d4fe1cda2a
ozkan <ozkandnz09@gmail.com>
2025-11-27 15:10:51 +0300

    sendVerificationMail

--- models/Otp.js
+++ models/Otp.js
@@ -27,8 +27,17 @@ otpSchema.pre("save", async function (next) {
   if (this.isNew) {
     const salt = await bcrypt.genSalt(10);
     await sendVerificationMail(this.email, this.otp, this.otp_type);
-    this.otp = await bcrypt.hash(this.otp, salt)
+    this.otp = await bcrypt.hash(this.otp, salt);
   }
 
   next();
 });
+
+async function sendVerificationMail(email, otp, otp_type) {
+  try {
+    const mailResponse = await mailSender(email, otp, otp_type);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
