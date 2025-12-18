0db3c5963b0f729d557f5898fbf31fa5ab088674
ozkan <ozkandnz09@gmail.com>
2025-12-19 00:16:03 +0300

    auth controller

---
+++ controllers/auth/auth.js
@@ -0,0 +1,43 @@
+import { StatusCode } from "http-status-code";
+import { BadRequestError, UnauthenticatednError } from "../../errors/index.js";
+import jwt from "jsonwebtoken";
+import User from "../../models/User.js";
+
+const register = async (req, res) => {
+  const { email, password, reqister_token } = req.body;
+  if (!email || !password || !reqister_token) {
+    throw new BadRqquestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new BadRequestError("User already exists");
+  }
+
+  try {
+    const payload = jwt.verify(reqister_token, process.env.REGISTER_SECRET);
+    if (payload.email !== email) {
+      throw new BadRequestError("Invalid register token");
+    }
+
+    const newUser = await User.create({ email, password });
+    const access_token = newUser.createAccessToken();
+    const refresh_token = newUser.createRefreshToken();
+    res.status(StatusCode.CREATED).json({
+      user: { email: newUser.email, userId: newUser.id },
+      tokens: { access_token, refresh_token },
+    });
+  } catch (error) {
+    console.error(error);
+    throw new BadRequestError("Invalid body");
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError("Please provide all values");
+  }
+
+  const user = await User.findOne({ email });
+};
