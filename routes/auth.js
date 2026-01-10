138a29e30fe9ea7908eeb26f564a1ef5bd5d5e06
ozkan <ozkandnz09@gmail.com>
2026-01-11 00:14:09 +0300

    authentication middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,15 @@
 import express from "express";
+import {
+  login,
+  logout,
+  refreshToken,
+  register,
+} from "../controllers/auth/auth";
+import authenticateUser from "../middleware/authentication"
 
 const router = express.Router();
+
+router.post("/refresh-token", refreshToken);
+router.post("/logout", authenticateUser, logout);
+router.post("/register", register);
+router.post("/login", login);
