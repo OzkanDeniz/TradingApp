66f5007dd1d13afe54576852ead2b0652754050f
ozkan <ozkandnz09@gmail.com>
2026-02-14 22:58:55 +0300

    switch case

--- controllers/auth/otp.js
+++ controllers/auth/otp.js
@@ -19,13 +19,26 @@ const otpRecord = await OTP.findOne({ email, otp_type })
   .sort({ createdAt: -1 })
   .limit(1);
 
-  if(!otpRecord){
-    throw new BadRequestError("Invalid OTP or OTP expired");
-  }
+if (!otpRecord) {
+  throw new BadRequestError("Invalid OTP or OTP expired");
+}
 
-  const isVerified = await otpRecord.compareOTP(otp)
-  if(!isVerified){
-    throw new BadRequestError("Invalid OTP or OTP expired")
-  }
+const isVerified = await otpRecord.compareOTP(otp);
+if (!isVerified) {
+  throw new BadRequestError("Invalid OTP or OTP expired");
+}
 
-  await OTP.findbyIdAndDelete(otpRecord.id)
+await OTP.findbyIdAndDelete(otpRecord.id);
+
+switch (otp_type) {
+  case "phone":
+    await User.findOneAndUpdate({ email }, { phone_number: data });
+    break;
+  case "reset pin":
+    if (!data || data.length != 4) {
+      throw new BadRequestError("PIN should be 4 Digit ");
+    }
+
+  default:
+    break;
+}
